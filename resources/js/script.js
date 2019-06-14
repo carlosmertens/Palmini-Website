d724e6448cadd806c3116427240a0f3d604f31a8
Carlos Mertens <mertenscarlos@yahoo.com>
2019-06-14 15:18:53 +0200

    feat: Add functionality to Show Me More button

--- resources/js/script.js
+++ resources/js/script.js
@@ -10,8 +10,12 @@ $(document).ready(function() {
     });
 
     /* Scroll on buttons */
-    $('.js--scroll-to-plans').click(function () {
+    $('.js--scroll-to-plans').click(function() {
         $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
 
 });
