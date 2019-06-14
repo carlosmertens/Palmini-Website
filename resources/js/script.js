2552e2a117ef76ccbea16498426a4e5c554ce86b
Carlos Mertens <mertenscarlos@yahoo.com>
2019-06-14 16:05:27 +0200

    feat: Add functionality to navigation links

--- resources/js/script.js
+++ resources/js/script.js
@@ -18,4 +18,17 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
 
+    /* Scroll on navigation links */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({scrollTop: target.offset().top}, 1000);
+                return false;
+                }
+            }
+        });
+    });
 });
